dbde5e6b23050826760fa223566b5168ab3c6be8
Alex Clowes <alexclowes@gmail.com>
2019-12-05 21:03:35 +0000

    2019 Problem 5 part 2

--- 2019/05/part_02.py
+++ 2019/05/part_02.py
@@ -4,7 +4,7 @@ from run import IntCodeComputer
 def main():
     with open("program.txt") as f:
         program = list(map(int, f.readline().split(",")))
-    IntCodeComputer(program).run(1)
+    IntCodeComputer(program).run(5)
 
 
 if __name__ == "__main__":
--- 2019/05/run.py
+++ 2019/05/run.py
@@ -1,80 +1,114 @@
 import operator
 
 
-def instruction_gen(program):
-    pos = 0
-    while True:
-        op_code, _ = decode(program[pos])
-        if op_code in [1, 2]:
-            yield program[pos : pos + 4]
-            pos += 4
-        elif op_code in [3, 4]:
-            yield program[pos : pos + 2]
-            pos += 2
-        elif op_code in [99]:
-            yield program[pos : pos + 1]
-            pos += 1
+class IntCodeComputer:
+    def __init__(self, program):
+        self.pos = 0
+        self.program = program
+
+    @staticmethod
+    def decode(instruction):
+        op_code = instruction % 100
+        instruction //= 100
+        param_modes = []
+        while instruction > 0:
+            param_modes.append(instruction % 10)
+            instruction //= 10
+        return op_code, param_modes
+
+    @staticmethod
+    def pad(modes, expected_len):
+        modes += [0] * (expected_len - len(modes))
+
+    def bin_op(self, param_modes, args, operator):
+        self.pad(param_modes, 3)
+        for i in range(2):
+            if param_modes[i] == 0:
+                args[i] = self.program[args[i]]
+        assert param_modes[2] == 0
+        self.program[args[2]] = operator(*args[:2])
+
+    def add(self, param_modes, args):
+        self.bin_op(param_modes, args, operator.add)
+    
+    def mult(self, param_modes, args):
+        self.bin_op(param_modes, args, operator.mul)
+
+    def read_in(self, param_modes, args, in_val):
+        self.pad(param_modes, 1)
+        assert param_modes[0] == 0
+        self.program[args[0]] = in_val
+
+    def write_out(self, param_modes, args):
+        self.pad(param_modes, 1)
+        if param_modes[0] == 0:
+            args[0] = self.program[args[0]]
+        print(args[0])
+
+    def jump_if_true(self, param_modes, args):
+        self.pad(param_modes, 2)
+        for i in range(2):
+            if param_modes[i] == 0:
+                args[i] = self.program[args[i]]
+        if args[0]:
+            self.pos = args[1]
         else:
-            raise ValueError(f"Unrecognised op code {op_code}")
+            self.pos += 3
 
-
-def decode(instruction):
-    op_code = instruction % 100
-    instruction //= 100
-    param_modes = []
-    while instruction > 0:
-        param_modes.append(instruction % 10)
-        instruction //= 10
-    return op_code, param_modes
-
-
-def pad(modes, expected_len):
-    modes += [0] * (expected_len - len(modes))
-
-
-def bin_op(program, param_modes, args, operator):
-    pad(param_modes, 3)
-    for i in range(2):
-        if param_modes[i] == 0:
-            args[i] = program[args[i]]
-    assert param_modes[2] == 0
-    program[args[2]] = operator(*args[:2])
-
-
-def add(program, param_modes, args):
-    bin_op(program, param_modes, args, operator.add)
-
-
-def mult(program, param_modes, args):
-    bin_op(program, param_modes, args, operator.mul)
-
-
-def read_in(program, param_modes, args, in_val):
-    pad(param_modes, 1)
-    assert param_modes[0] == 0
-    program[args[0]] = in_val
-
-
-def write_out(program, param_modes, args):
-    pad(param_modes, 1)
-    if param_modes[0] == 0:
-        args[0] = program[args[0]]
-    print(args[0])
-
-
-def run(program, in_val):
-    for instruction in instruction_gen(program):
-        op, *args = instruction
-        op_code, param_modes = decode(op)
-        if op_code == 1:
-            add(program, param_modes, args)
-        elif op_code == 2:
-            mult(program, param_modes, args)
-        elif op_code == 3:
-            read_in(program, param_modes, args, in_val)
-        elif op_code == 4:
-            write_out(program, param_modes, args)
-        elif op_code == 99:
-            break
+    def jump_if_false(self, param_modes, args):
+        self.pad(param_modes, 2)
+        for i in range(2):
+            if param_modes[i] == 0:
+                args[i] = self.program[args[i]]
+        if not args[0]:
+            self.pos = args[1]
         else:
-            raise ValueError(f"Unrecognised op code {op_code}")
+            self.pos += 3
+
+    def less_than(self, param_modes, args):
+        self.bin_op(param_modes, args, lambda x, y: int(x < y))
+
+    def equals(self, param_modes, args):
+        self.bin_op(param_modes, args, lambda x, y: int(x == y))
+
+    def instructions(self):
+        while True:
+            op_code, _ = self.decode(self.program[self.pos])
+            if op_code in [1, 2, 7, 8]:
+                yield self.program[self.pos : self.pos + 4]
+                self.pos += 4
+            elif op_code in [3, 4]:
+                yield self.program[self.pos : self.pos + 2]
+                self.pos += 2
+            elif op_code in [5, 6]:
+                yield self.program[self.pos : self.pos + 3]
+            elif op_code in [99]:
+                yield self.program[self.pos : self.pos + 1]
+                self.pos += 1
+            else:
+                raise ValueError(f"Unrecognised op code {op_code}")
+
+    def run(self, in_val):
+        for instruction in self.instructions():
+            op, *args = instruction
+            op_code, param_modes = self.decode(op)
+            if op_code == 1:
+                self.add(param_modes, args)
+            elif op_code == 2:
+                self.mult(param_modes, args)
+            elif op_code == 3:
+                self.read_in(param_modes, args, in_val)
+            elif op_code == 4:
+                self.write_out(param_modes, args)
+            elif op_code == 5:
+                self.jump_if_true(param_modes, args)
+            elif op_code == 6:
+                self.jump_if_false(param_modes, args)
+            elif op_code == 7:
+                self.less_than(param_modes, args)
+            elif op_code == 8:
+                self.equals(param_modes, args)
+            elif op_code == 99:
+                break
+            else:
+                raise ValueError(f"Unrecognised op code {op_code}")
