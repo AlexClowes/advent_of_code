de0fd99194cb69dc6d7ad2072032b146c8a5fe45
AlexClowes <alexclowes@gmail.com>
2020-08-04 09:22:05 +0100

    2015 Problem 21

---
+++ 2015/21/boss_fight.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+from itertools import chain, combinations
+
+
+Item = namedtuple("Item", ["cost", "damage", "armor"])
+
+weapons = [
+    Item(8, 4, 0),
+    Item(10, 5, 0),
+    Item(25, 6, 0),
+    Item(40, 7, 0),
+    Item(74, 8, 0),
+]
+armors = [
+    Item(0, 0, 0),
+    Item(13, 0, 1),
+    Item(31, 0, 2),
+    Item(53, 0, 3),
+    Item(75, 0, 4),
+    Item(102, 0, 5),
+]
+rings = [
+    Item(0, 0, 0),
+    Item(0, 0, 0),
+    Item(25, 1, 0),
+    Item(50, 2, 0),
+    Item(100, 3, 0),
+    Item(20, 0, 1),
+    Item(40, 0, 2),
+    Item(80, 0, 3),
+]
+
+
+def player_wins(items, boss_stats):
+    boss_health, boss_damage, boss_armor = 109, 8, 2
+    player_damage = sum(item.damage for item in items)
+    player_armor = sum(item.armor for item in items)
+    player_health = 100
+    while True:
+        # Player attacks
+        boss_health -= max(1, player_damage - boss_armor)
+        if boss_health < 0:
+            return True
+        # Boss attacks
+        player_health -= max(1, boss_damage - player_armor)
+        if player_health < 0:
+            return False
+
+
+def main():
+    boss_stats = {"health": 109, "damage": 8, "armor": 2}
+    min_cost = float("inf")
+    for weapon in weapons:
+        for armor in armors:
+            for ring1, ring2 in combinations(rings, 2):
+                items = (weapon, armor, ring1, ring2)
+                cost = sum(item.cost for item in items)
+                if cost < min_cost and player_wins(items, boss_stats):
+                    min_cost = cost
+    print(min_cost)
+
+
+if __name__ == "__main__":
+    main()
