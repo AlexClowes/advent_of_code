e6f63ec98bc9b416f11b75e46b8140c15b00c3f5
AlexClowes <alexclowes@gmail.com>
2020-08-04 18:38:15 +0100

    2015 Problem 24

---
+++ 2015/24/balance.py
@@ -0,0 +1,33 @@
+from itertools import combinations
+
+
+def product(iterable):
+    ret = 1
+    for el in iterable:
+        ret *= el
+    return ret
+
+
+def gen_groups(target, weights):
+    for i in range(1, len(weights) + 1):
+        candidates = (comb for comb in combinations(weights, i) if sum(comb) == target)
+        yield from sorted(candidates, key=product)
+
+
+def main():
+    with open("weights.txt") as f:
+        weights = [int(line.strip()) for line in f][::-1]
+    weights = weights
+    target_weight = sum(weights) // 3
+
+    group1_candidates = gen_groups(target_weight, weights)
+    for group1 in group1_candidates:
+        unused = [w for w in weights if w not in group1]  # Assumes no duplicate weights
+        for group2 in gen_groups(target_weight, unused):
+            # If group 2 exists, then so does group 3 and we are done
+            print(product(group1))
+            return
+
+
+if __name__ == "__main__":
+    main()
