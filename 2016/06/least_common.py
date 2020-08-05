a4abd57ab7dc22259b3a0ace9497e5842c3c0432
AlexClowes <alexclowes@gmail.com>
2020-08-05 21:59:04 +0100

    2016 Problem 6

---
+++ 2016/06/least_common.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+def decode(messages):
+    counts = [Counter(m[i] for m in messages) for i in range(len(messages[0]))]
+    return "".join(c.most_common()[-1][0] for c in counts)
+
+
+def main():
+    with open("messages.txt") as f:
+        messages = [line.strip() for line in f]
+    print(decode(messages))
+
+
+if __name__ == "__main__":
+    main()
