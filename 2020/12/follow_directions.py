4905c46fdf5d45afcc4265618ceb1b2c84a24ea7
Alex Clowes <alexclowes@gmail.com>
2020-12-12 15:11:06 +0000

    2020 Problem 12 - use complex to simplify code and remove numpy dependency

--- 2020/12/follow_directions.py
+++ 2020/12/follow_directions.py
@@ -1,43 +1,28 @@
-import numpy as np
-
-
-def rotate_anticlockwise(heading, angle):
-    if angle == 90:
-        heading[0], heading[1] = -heading[1], heading[0]
-    elif angle == 180:
-        heading[:] = -heading
-    elif angle == 270:
-        heading[0], heading[1] = heading[1], -heading[0]
-    else:
-        raise ValueError(f"Expected angle to be one of 90, 180, 270, but got {angle}")
-    return heading
-
-
 def main():
     with open("directions.txt") as f:
         directions = [(line[0], int(line.strip()[1:])) for line in f]
 
-    heading = np.array([1, 0])
-    position = np.array([0, 0])
+    heading = complex(1)
+    position = complex(0)
 
     displacement = {
-        "N": np.array([0, 1]),
-        "S": np.array([0, -1]),
-        "E": np.array([1, 0]),
-        "W": np.array([-1, 0]),
+        "N": complex(0, 1),
+        "S": complex(0, -1),
+        "E": complex(1, 0),
+        "W": complex(-1, 0),
     }
     for op, operand in directions:
         if op in "NSEW":
             position += operand * displacement[op]
         elif op == "L":
-            rotate_anticlockwise(heading, operand)
+            heading *= complex(0, 1) ** (operand // 90)
         elif op == "R":
-            rotate_anticlockwise(heading, 360 - operand)
+            heading *= complex(0, -1) ** (operand // 90)
         elif op == "F":
             position += operand * heading
         else:
             raise ValueError(f"Unrecognised op {op}")
-    print(sum(abs(position)))
+    print(abs(position.real) + abs(position.imag))
 
 
 if __name__ == "__main__":
