1cf9359e5f24f873c0421b25dec3b2c2b17b29a2
Alex Clowes <alexclowes@gmail.com>
2020-12-19 13:59:07 +0000

    2020 Problem 19 - remove re dependency

--- 2020/19/match_rules.py
+++ 2020/19/match_rules.py
@@ -1,32 +1,50 @@
-import re
-
-
-def build_pattern(rules):
-    pattern = rules["0"]
-    while True:
-        for idx, token in enumerate(pattern):
-            if token.isdecimal():
-                pattern = pattern[:idx] + rules[token] + pattern[idx + 1 :]
-                break
-        else:
-            break
-    return "^" + "".join(pattern) + "$"
+def try_int(x):
+    try:
+        return int(x)
+    except ValueError:
+        return x
 
 
 def main():
-    rules = {}
     with open("rules_messages.txt") as f:
+        rules = {}
         while (line := f.readline()) != "\n":
             rule_idx, rule = line.strip().split(": ")
-            rule = rule.replace('"', "")
-            if "|" in rule:
-                rule = f"( {rule} )"
-            rules[rule_idx] = rule.split(" ")
+            rules[int(rule_idx)] = tuple(
+                tuple(map(try_int, t.split(" ")))
+                for t in rule.replace('"', "").split(" | ")
+            )
 
         messages = [line.strip() for line in f]
 
-    pattern = build_pattern(rules)
-    print(sum(bool(re.match(pattern, msg)) for msg in messages))
+    def recurse(rule, message):
+        # Base case
+        if not rule and not message:
+            return True, None
+        if (rule and not message) or (not rule and message):
+            return False, None
+
+        for option in rule:
+            match = True
+            pos = 0
+            for element in option:
+                if element == "a" or element == "b":
+                    match = element == message[pos]
+                    pos_inc = 1
+                else:
+                    match, pos_inc = recurse(rules[element], message[pos:])
+                if not match:
+                    break
+                pos += pos_inc
+            else:
+                break
+        return match, pos
+
+    def match(message):
+        match, length = recurse(rules[0], message)
+        return match and length == len(message)
+
+    print(sum(map(match, messages)))
 
 
 if __name__ == "__main__":
