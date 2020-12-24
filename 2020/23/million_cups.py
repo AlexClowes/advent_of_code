916be77138824656fd99f28bf5576626a6ae8da9
Alex Clowes <alexclowes@gmail.com>
2020-12-24 09:24:21 +0000

    2020 Problem 23 - simplify part 2 code

--- 2020/23/million_cups.py
+++ 2020/23/million_cups.py
@@ -2,15 +2,9 @@ from itertools import chain
 
 
 class Node:
-    def __init__(self, val, lookup=None, next=None, prev=None):
+    def __init__(self, val, lookup):
         self.val = val
-        if lookup is not None:
-            lookup[val] = self
-        self.next = None
-        self.prev = prev
-
-    def __repr__(self):
-        return f"Node({self.val})"
+        lookup[val] = self
 
 
 def main():
@@ -21,10 +15,9 @@ def main():
     cup_lookup = {}
     node = first_node = Node(next(cup_vals), lookup=cup_lookup)
     for val in cup_vals:
-        node.next = Node(val, lookup=cup_lookup, prev=node)
+        node.next = Node(val, lookup=cup_lookup)
         node = node.next
     node.next = first_node
-    first_node.prev = node
 
     # Get min and max cup vals
     max_cup = 10 ** 6
@@ -35,7 +28,6 @@ def main():
         # Remove three cups after current cup
         three_cups = current_cup.next
         current_cup.next = current_cup.next.next.next.next
-        current_cup.next.prev = current_cup
 
         # Get label of destination cup
         dest = current_cup.val - 1 or max_cup
@@ -44,10 +36,8 @@ def main():
         dest_cup = cup_lookup[dest]
 
         # Place 3 cups after destination cup
-        dest_cup.next.prev = three_cups.next.next
         three_cups.next.next.next = dest_cup.next
         dest_cup.next = three_cups
-        three_cups.prev = dest_cup
 
         # Get new current cup
         current_cup = current_cup.next
