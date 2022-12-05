7e535585967e6fa9df0861c3616de5ef9e2237be
Alex Clowes <alexclowes@gmail.com>
2022-12-05 19:20:40 +0000

    2022 Problem 5

---
+++ 2022/05/single_pick.py
@@ -0,0 +1,39 @@
+from collections import defaultdict, deque
+import re
+
+
+def get_stacks(file):
+    crate_pattern = r"(\s{4}|\[[A-Z]\])"
+    stacks = defaultdict(deque)
+    for line in file:
+        crates = [
+            None if s.isspace() else s[1]
+            for s in re.findall(crate_pattern, " " + line)
+        ]
+        if not any(crates):
+            break
+        for i, crate in enumerate(crates, 1):
+            if crate:
+                stacks[i].appendleft(crate)
+    return stacks
+
+
+def get_moves(file):
+    move_pattern = r"move (\d+) from (\d) to (\d)"
+    for line in file:
+        yield map(int, re.match(move_pattern, line).groups())
+
+
+def main():
+    with open("starting_stacks.txt") as f:
+        stacks = get_stacks(f)
+        next(f)  # Skip blank line
+        for count, src, dest in get_moves(f):
+            for _ in range(count):
+                stacks[dest].append(stacks[src].pop())
+
+    print("".join(stacks[i + 1][-1] for i in range(len(stacks))))
+
+
+if __name__ == "__main__":
+    main()
