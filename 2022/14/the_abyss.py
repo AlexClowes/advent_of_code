3aedffa299bced4982fcd6b7511b3cfc421b5c20
Alex Clowes <alexclowes@gmail.com>
2022-12-14 19:36:36 +0000

    2022 Problem 14

---
+++ 2022/14/the_abyss.py
@@ -0,0 +1,49 @@
+from itertools import pairwise
+
+
+def get_range(lo, hi):
+    return range(min(lo, hi), max(lo, hi) + 1)
+
+
+def get_rocks(scan_line):
+    for (x1, y1), (x2, y2) in pairwise(
+        tuple(map(int, vertex.split(","))) for vertex in scan_line.split(" -> ")
+    ):
+        if x1 == x2:
+            yield from ((x1, y) for y in get_range(y1, y2))
+        else:
+            yield from ((x, y1) for x in get_range(x1, x2))
+
+        
+def main():
+    blocked = set()
+    with open("rock_scan.txt") as f:
+        for scan_line in f:
+            for rock in get_rocks(scan_line):
+                blocked.add(rock)
+
+    ymax = max(y for _, y in blocked)
+
+    def get_next_resting_place():
+        x, y = 500, 0
+        while True:
+            if y > ymax:
+                return None
+            for dx in (0, -1, 1):
+                if (x + dx, y + 1) not in blocked:
+                    x, y = x + dx, y + 1
+                    break
+            else:
+                break
+        return x, y
+
+    count = 0
+    while (resting_place := get_next_resting_place()) is not None:
+        blocked.add(resting_place)
+        count += 1
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
