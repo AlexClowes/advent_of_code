54c8f591dc6fa17d68a9fdce9e919e9c6b6993aa
Alex Clowes <alexclowes@gmail.com>
2024-10-26 18:27:16 +0100

    2023 Problem 9

---
+++ 2023/09/extrapolate.py
@@ -0,0 +1,15 @@
+def extrapolate(sequence):
+    if all(n == 0 for n in sequence):
+        return 0
+    diffs = [a - b for a, b in zip(sequence[1:], sequence)]
+    return sequence[-1] + extrapolate(diffs)
+
+
+def main():
+    with open("report.txt") as f:
+        observations = ([int(n) for n in line.strip().split()] for line in f)
+        print(sum(extrapolate(observation) for observation in observations))
+
+
+if __name__ == "__main__":
+    main()
